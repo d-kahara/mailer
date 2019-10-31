fb0f7150b1d0b0b59c8f250885e603f89e1c6600
David Kahara <kaharadave@gmail.com>
2019-10-31 21:30:10 +0300

    (data-upload): implement google sheet parsing
    -configure xlsx
    -style templates

--- api/server/routes/UserRoutes.js
+++ api/server/routes/UserRoutes.js
@@ -1,9 +1,12 @@
 import { Router } from 'express';
 import UserController from '../controllers/UserController';
+import Usercontroller from '../controllers/UserController';
 
 const router = Router();
 
 router.post('/', UserController.addUser);
-router.get('/', UserController.sendEmail);
+router.get('/sendEmail', UserController.sendEmail);
+router.get('/', UserController.getAllUsers);
+router.get('/uploadSheetData', Usercontroller.uploadSheetData);
 
 export default router;
---
+++ api/server/utils/googleSheetParser.js
@@ -0,0 +1,13 @@
+const xlsx = require('xlsx');
+const path = require('path');
+
+const workAssessment = xlsx.readFile(path.resolve('sheet.xlsx'));
+
+/*
+* Parse a specific sheet in the workbook
+*/
+const currentWorksheet = workAssessment.Sheets['Sheet1'];
+/*
+* Parse xlsx record as Json data
+*/
+export default xlsx.utils.sheet_to_json(currentWorksheet);
